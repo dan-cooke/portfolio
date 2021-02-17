4fcc2d423269a6b5f6f4ee04187445391bf31fec
dan-cooke <danielcooke@1996@gmail.com>
2021-02-17 13:40:13 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: "dan.",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
