8c72464208b5163f8034f4e272cd969f82db3594
Daniel Cooke <danielcooke1996@gmail.com>
2021-05-28 23:53:44 +0100

    Update employment status

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -4,20 +4,13 @@ import './Splash.scss'
 import SplashBackground from './SplashBackground'
 
 const Splash = () => {
-	const getDate = () => {
-		let date = new Date()
-		return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-	}
 	return (
 		<SplashBackground className='splash'>
 			<Avatar className='splash__avatar' />
 			<span className='splash__bio'>
 				<h1>Daniel Cooke.</h1>
 				<h1>Frontend Developer.</h1>
-				<h1>
-					Looking for work
-					<span className='last_updated'>as of {getDate()}</span>
-				</h1>
+				<h1>Employeed 😀 </h1>
 			</span>
 		</SplashBackground>
 	)
