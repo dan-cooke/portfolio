2dcb932f801fe1489a1bef8fb2700bc14c837a65
Daniel Cooke <danielcooke1996@gmail.com>
2023-03-20 15:03:51 +0000

    Update CV

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@ export const Navbar = () => {
   return (
     <nav className="py-4 fixed flex w-full lg:mr-8 lg:mr-32 md:place-content-end place-content-center">
       <a
-        href="/Daniel_Cookes_Resume_October_2022.pdf"
+        href="/Dans Latest CV.pdf"
         target="_blank"
         className="font-mono text-white border border-white rounded-sm p-2 px-4 hover:text-primary hover:border-primary"
       >
